0bc0b13e75516e8c7542ac8534f5de39e1b16c2a
Toby Ferguson <toby.ferguson@oracle.com>
2014-12-12 00:11:07 -0800

    Vagrantfile & setup that will work using user vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -7,8 +7,8 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ol6minimal"
   config.vm.hostname = "oemrepo.lab.net"
-  config.ssh.username = "oracle"
-  config.ssh.forward_x11 = "true"
+  # config.ssh.username = "oracle"
+  # config.ssh.forward_x11 = "true"
   config.vm.network "private_network", ip: "192.168.50.4"
   config.vm.provision "shell", path: "setup.sh", privileged: false
 end
--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-sudo yum -y update
+#sudo yum -y update
 
 sudo yum -y install unzip oracle-rdbms-server-11gR2-preinstall
 
-echo oracle | passwd --stdin
-sudo echo -e 'oracle\tALL=(ALL)\tNOPASSWD: ALL' >/etc/sudoers.d/oracle
-sudo cp -r ~vagrant/.ssh ~oracle
-sudo chown -R oracle:oinstall ~oracle
-
-cat <<EOF  >>/home/oracle/.bash_profile
+sudo cat <<EOF  >>/home/oracle/.bash_profile
 export ORACLE_BASE=/u01/app/oracle 
 export ORACLE_HOME=\${ORACLE_BASE:?}/product/11.2.0/db_1
 export ORACLE_SID=oemrepo
@@ -26,26 +21,20 @@ unzip -u -d /vagrant p10404530_112030_Linux-x86-64_1of7.zip
 unzip -u -d /vagrant p10404530_112030_Linux-x86-64_2of7.zip
 }
 
-/vagrant/database/runInstaller -silent -ignorePrereq -responseFile /vagrant/db.rsp -waitforcompletion
 
-netca -silent -responseFile $ORACLE_HOME/assistants/netca/netca.rsp
+sudo "su -c /vagrant/database/runInstaller -silent -ignorePrereq -responseFile /vagrant/db.rsp -waitforcompletion - oracle"
 
-unzip -u -d ${ORACLE_HOME:?}/assistants/dbca/templates /vagrant/11.2.0.3_Database_Template_for_EM12_1_0_4_Linux_x64.zip
+sudo su -c 'netca -silent -responseFile $ORACLE_HOME/assistants/netca/netca.rsp' - oracle
 
-dbca -silent -createDatabase -responseFile /vagrant/dbca.rsp
+sudo su -c 'unzip -u -d ${ORACLE_HOME:?}/assistants/dbca/templates /vagrant/11.2.0.3_Database_Template_for_EM12_1_0_4_Linux_x64.zip' - oracle
+
+sudo su -c 'dbca -silent -createDatabase -responseFile /vagrant/dbca.rsp' - oracle
 
 sudo cp /vagrant/dbora /etc/init.d
 sudo chmod 755 /etc/init.d/dbora
 sudo chkconfig --add dbora
 
-if [[ -f /etc/oratab ]]
-then
-    sed -i 's/N$/Y/' /etc/oratab
-else
-    echo "$ORACLE_SID:$ORACLE_HOME:Y" >/tmp/oratab
-    sudo mv /tmp/oratab /etc
-fi    
+sudo sed -i 's/N$/Y/' /etc/oratab
+sudo service dbora start
 
-service dbora start
-
-sqlplus / as sysdba @/vagrant/patch.sql
+sudo su -c 'sqlplus / as sysdba @/vagrant/patch.sql' - oracle
